421326b2e25b50ef07d6b57cdda4ecc745e4d872
mtrupiano <markt6749@gmail.com>
2020-12-17 17:02:16 -0800

    finished rendering blocks and task saving logic

--- js/script.js
+++ js/script.js
@@ -7,20 +7,22 @@ $(document).ready(function() {
     var month = moment().month(now._d.getMonth()).format("MMMM");
     $("#currentDay").text(`${today}, ${month} ${date}`);
 
-    var lastUpdate;
-
+    var lastUpdate = localStorage.getItem("lastUpdate") ? parseInt(localStorage.getItem("lastUpdate")) : 0;
+    console.log(now._d.getDay());
+    
+    var tasks;
     if (lastUpdate) {
         if (lastUpdate !== now._d.getDay()) {
             console.log("New day, clearing tasks");
+            tasks = ["", "", "", "", "", "", "", "", ""];
+            localStorage.setItem("tasks", JSON.stringify(tasks));
             // clearTasks();
         }
     }
 
-    renderBlocks();
-
-
+    
+    
     // Pull saved tasks from local storage
-    var tasks;
     var tRead;
     if (tRead = localStorage.getItem("tasks")) {
         tasks = JSON.parse(tRead);
@@ -28,6 +30,7 @@ $(document).ready(function() {
         tasks = ["", "", "", "", "", "", "", "", ""];
         localStorage.setItem("tasks", JSON.stringify(tasks));
     }
+    renderBlocks();
 
     console.log(tasks);
     $(".saveBtn").click(function(event) {
@@ -38,18 +41,43 @@ $(document).ready(function() {
         // save taskDescr to local storage of tasks for the day
         tasks[hour - 9] = taskDescr;
         localStorage.setItem("tasks", JSON.stringify(tasks));
-        lastUpdate = 5 //moment()._d.getDay();
+        localStorage.setItem("lastUpdate", moment()._d.getDay());
         console.log(lastUpdate + " " + now._d.getDay())
+        renderBlocks();
     });
 
     // Function to draw all time blocks and load any saved task content
     function renderBlocks() {
+        var timeBlockContainer = $(".container");
+        timeBlockContainer.html("");
+        var iDay;
         for (var i = 0; i < 9; i++) {
+            iDay = i + 9 > 12 ? (i + 9) % 12 : i + 9
+            var newRow = $("<div>");
+            newRow.addClass("row");
+
             var newBlock = $("<div>");
             newBlock.addClass("input-group");
             newBlock.addClass("time-block");
 
-            
+            var newBlockLabel = $("<label>").addClass("hour");
+            newBlockLabel.text(`${iDay} ${i + 9 >= 12 ? "PM" : "AM"}`);
+
+            var newBlockTextArea = $("<textarea>").addClass("description");
+            newBlockTextArea.attr("id", `descr-${iDay}`);
+            newBlockTextArea.attr("cols", "30");
+            newBlockTextArea.attr("rows", "4");
+            newBlockTextArea.text(tasks[i]);
+
+            var newBlockSaveBtn = $("<button>").addClass("saveBtn");
+            newBlockSaveBtn.attr("data-hour", `${iDay}`)
+            newBlockSaveBtn.text("Save");
+
+            newBlock.append(newBlockLabel);
+            newBlock.append(newBlockTextArea);
+            newBlock.append(newBlockSaveBtn);
+            newRow.append(newBlock);
+            timeBlockContainer.append(newRow);
         }
     }
 
